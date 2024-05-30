5f68efc2003fba0a6711db6e454a1938c2e2ec0f
rizik2105 <168979695+rizik2105@users.noreply.github.com>
2024-05-30 23:01:32 +0530

    Update borrow.php

--- borrow.php
+++ borrow.php
@@ -1,50 +1,31 @@
+<?php
+session_start();
+
+// Initialize the number of books if not set
+if (!isset($_SESSION['number_of_books'])) {
+    $_SESSION['number_of_books'] = 10; // starting with 10 books
+}
+
+// Check if the button is clicked
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['decrease'])) {
+        if ($_SESSION['number_of_books'] > 0) {
+            $_SESSION['number_of_books']--;
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <style>
-    .button {
-      display: inline-block;
-      padding: 10px 20px;
-      font-size: 16px;
-      cursor: pointer;
-      text-align: center;
-      text-decoration: none;
-      outline: none;
-      color: #fff;
-      background-color: #4CAF50;
-      border: none;
-      border-radius: 15px;
-      box-shadow: 0 4px #999;
-      margin-right: 10px; /* Adds space between buttons */
-    }
-
-    .button:hover {background-color: #3e8e41}
-
-    .button:active {
-      background-color: #3e8e41;
-      box-shadow: 0 2px #666;
-      transform: translateY(2px);
-    }
-
-    .form-inline {
-      display: inline-block;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <title>Book Counter</title>
 </head>
 <body>
-  <table>
-    <tr>
-      <td>
-        <form action='borrow.php' method='post' class="form-inline">
-          <input type='hidden' value='<?php echo $row["sno"]; ?>' name='sno'>
-          <input type='submit' value='Borrow' class="button">
-        </form>
-        <form action='location.php' method='post' class="form-inline">
-          <input type='hidden' value='<?php echo $row["sno"]; ?>' name='sno'>
-          <input type='submit' value='Location' class="button">
-        </form>
-      </td>
-    </tr>
-  </table>
+    <h1>Number of Books: <?php echo $_SESSION['number_of_books']; ?></h1>
+    <form method="post">
+        <button type="submit" name="decrease">Decrease Number of Books</button>
+    </form>
 </body>
 </html>
